bb2dcd25b8f49e8d3c8a916e8e9b0d822b36a4da
Bianca Montalvo <biancamontalvo@biancas-air-2.mynetworksettings.com>
2026-05-09 19:37:58 -0700

    Java Beg

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-// http://www.omdbapi.com/?apikey=[f5e43e2a]&
-// http://img.omdbapi.com/?apikey=[f5e43e2a]&
+// https://www.omdbapi.com/?apikey=f5e43e2a&
+// https://img.omdbapi.com/?apikey=f5e43e2a&
+
+const movieListEl = document.querySelector(".movie-list");
+
+async function main() {
+    const movies = await fetch ("https://www.omdapi.com/?aipkey=f5e4332a&");
+    const moviesData = await movies.json();
+    movieListEl.innerHTML = moviesData.map((movie) => movieHTML(movie)).join("");
+}
+
+main();
+
+function showMovieInfo(imdbID) {
+    localStorage.setItem("imdbID", imdbID);
+    window.location.href = `${window.location.origin}/movie.html`
+    console.log(imdbID)
+}
+
+function movieHTML(movie) {
+    return `<div class="movie" onClick="showMovieInfo(${movie.imdbID})>
+                        <div class=${movie.poster}"movie-poster">
+                            <div class="movie__info--container">
+                                <h3>${movie.title}Movie Title:</h3>
+                                <p><b>Release Year:</b>${movie.year}2000</p>
+                                <p><b>Media Type:</b>${movie.type} movie</p>
+                            </div>
+                        </div>
+                    </div>`
+}
