e88c1baa0176c5c653d08ea65386c3d030d1dccf
Bianca Montalvo <biancamontalvo@biancas-air-2.mynetworksettings.com>
2026-05-16 10:42:35 -0700

    data fetching

--- script.js
+++ script.js
@@ -4,9 +4,9 @@
 const movieListEl = document.querySelector(".movie-list");
 
 async function main() {
-    const movies = await fetch ("https://www.omdapi.com/?apikey=f5e4332a&s=avengers");
+    const movies = await fetch ("https://www.omdbapi.com/?apikey=f5e43e2a&s=avengers");
     const moviesData = await movies.json();
-    movieListEl.innerHTML = moviesData.Search.map(.)
+    movieListEl.innerHTML = moviesData.Search.map(movieHTML).join("")
 }
 
 main();
@@ -18,12 +18,12 @@ function showMovieInfo(imdbID) {
 }
 
 function movieHTML(movie) {
-    return `<div class="movie" onClick="showMovieInfo(`${movie.imdbID}`)">
-                        <img src"${movie.Poster}" class="movie-poster">
+    return `<div class="movie" onClick="showMovieInfo('${movie.imdbID}')">
+                        <img src="${movie.Poster}" class="movie-poster">
                             <div class="movie__info--container">
-                                <h3>${movie.Title}Movie Title:</h3>
-                                <p><b>Release Year:</b>${movie.Year}2000</p>
-                                <p><b>Media Type:</b>${movie.Type} movie</p>
+                                <h3>Title: ${movie.Title}</h3>
+                                <p><b>Release Year: </b>${movie.Year}</p>
+                                <p><b>Media Type: </b>${movie.Type}</p>
                             </div>
                         </div>
                     </div>`
