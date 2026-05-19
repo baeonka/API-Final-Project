82c7cfc6c0db83ef1e0b1cb59936d42c858c29cc
Bianca Montalvo <biancamontalvo@biancas-air-2.mynetworksettings.com>
2026-05-19 15:11:35 -0700

    Sorting Complete

--- script.js
+++ script.js
@@ -2,14 +2,13 @@
 // https://img.omdbapi.com/?apikey=f5e43e2a&
 
 const movieListEl = document.querySelector(".movie-list");
+let movies;
 
-async function main() {
+async function getMovies() {
     const movies = await fetch ("https://www.omdbapi.com/?apikey=f5e43e2a&s=avengers");
     const moviesData = await movies.json();
-    movieListEl.innerHTML = moviesData.Search.map(movieHTML).join("")
+    return moviesData.Search;
 }
-
-main();
 
 function showMovieInfo(imdbID) {
     localStorage.setItem("imdbID", imdbID);
@@ -17,28 +16,12 @@ function showMovieInfo(imdbID) {
     console.log(imdbID)
 }
 
-function movieHTML(movie) {
-    return `<div class="movie" onClick="showMovieInfo('${movie.imdbID}')">
-                        <img src="${movie.Poster}" class="movie-poster">
-                            <div class="movie__info--container">
-                                <h3>Title: ${movie.Title}</h3>
-                                <p><b>Release Year: </b>${movie.Year}</p>
-                                <p><b>Media Type: </b>${movie.Type}</p>
-                            </div>
-                        </div>
-                    </div>`
-}
-
 function filterMovies(event) {
-    const selectedOption = event.target.value;
-    console.log(selectedOption);
+    renderMovies(event.target.value)
 }
-
-let movies;
 
 async function renderMovies(filter) {
-    const moviesWrapper = document.querySelector('.movies');
-
+    const moviesWrapper = document.querySelector('.movie-list');
     moviesWrapper.classList += " movies__loading";
 
     if (!movies) {
@@ -47,10 +30,10 @@ async function renderMovies(filter) {
     moviesWrapper.classList.remove('movies__loading')
 
     if (filter === 'NEW_TO_OLD') {
-        movies.sort((a, b) => b.releaseYear - a.releaseYear);
+        movies.sort((a, b) => parseInt(b.Year) - parseInt(a.Year));
     }
     else if (filter === 'OLD_TO_NEW') {
-        movies.sort((a, b) => a.releaseYear - b.releaseYear);
+        movies.sort((a, b) => parseInt(a.Year) - parseInt(b.Year));
     }
     else if (filter === 'A_TO_Z') {
         movies.sort((a, b) => a.Title.localeCompare(b.Title));
@@ -61,16 +44,17 @@ async function renderMovies(filter) {
 
     const moviesHTML = movies
     .map((movie) => {
-        return `<div class="movie">
-                        <div class="movie-poster">
-                            <img src="${movie.Poster}" class="poster-image" alt="${movie.Title}">
+        return `<div class="movie" onClick="showMovieInfo('${movie.imdbID}')">
+                        <img src="${movie.Poster}" class="movie-poster">
                             <div class="movie__info--container">
-                                <h3>Title:${movie.Title}</h3>
-                                <p><b>Release Year:</b>${movie.Year}</p>
-                                <p><b>Media Type:</b>${movie.Type}</p>
-                                </div>
-                            </div>`;
+                                <h3>Title: ${movie.Title}</h3>
+                                <p><b>Release Year: </b>${movie.Year}</p>
+                                <p><b>Media Type: </b>${movie.Type}</p>
+                            </div>
+                        </div>`
     }).join('');
 
     moviesWrapper.innerHTML = moviesHTML;
 }
+
+renderMovies();
