315951c1f53f0e1fabccbc2ceb05a40f8c3d230b
Bianca Montalvo <biancamontalvo@biancas-air-2.mynetworksettings.com>
2026-05-21 14:40:16 -0700

    java cont.

--- script.js
+++ script.js
@@ -2,79 +2,77 @@
 // https://img.omdbapi.com/?apikey=f5e43e2a&
 
 const movieListEl = document.querySelector(".movie-list");
-const moviesWrapper =document.querySelector('movie-list');
+const moviesWrapper = document.querySelector('.movie-list');
 const loadingSpinner = document.querySelector('.movies__loading--spinner');
 let movies;
 
-async function getMovies(searchTerm = "avengers") {
-    const movies = await fetch (`https://www.omdbapi.com/?apikey=f5e43e2a&s=${searchTerm}`);
+async function getMovies(searchTerm = 'avengers') {
+    const movies = await fetch(`https://www.omdbapi.com/?apikey=f5e43e2a&s=${searchTerm}`);
     const moviesData = await movies.json();
     return moviesData.Search;
 }
 
 function showMovieInfo(imdbID) {
     localStorage.setItem("imdbID", imdbID);
-    window.location.href = `${window.location.origin}/index.html`
-    console.log(imdbID)
+    window.location.href = `${window.location.origin}/index.html`;
+    console.log(imdbID);
 }
 
 function filterMovies(event) {
-    renderMovies(event.target.value)
+    renderMovies(event.target.value);
 }
 
 async function renderMovies(filter) {
-    loadingSpinner.classList += " movies__loading";
+    loadingSpinner.classList.add("movies__loading");
 
     if (!movies) {
         movies = await getMovies();
     }
     if (filter === 'NEW_TO_OLD') {
         movies.sort((a, b) => parseInt(b.Year) - parseInt(a.Year));
-    }
-    else if (filter === 'OLD_TO_NEW') {
+    } else if (filter === 'OLD_TO_NEW') {
         movies.sort((a, b) => parseInt(a.Year) - parseInt(b.Year));
-    }
-    else if (filter === 'A_TO_Z') {
+    } else if (filter === 'A_TO_Z') {
         movies.sort((a, b) => a.Title.localeCompare(b.Title));
-    }
-    else if (filter === 'Z_TO_A') {
+    } else if (filter === 'Z_TO_A') {
         movies.sort((a, b) => b.Title.localeCompare(a.Title));
     }
 
     const moviesHTML = movies
-    .map((movie) => {
-        return `<div class="movie" onClick="showMovieInfo('${movie.imdbID}')">
+        .map((movie) => {
+            return `<div class="movie" onClick="showMovieInfo('${movie.imdbID}')">
                         <img src="${movie.Poster}" class="movie-poster">
                             <div class="movie__info--container">
                                 <h3>Title: ${movie.Title}</h3>
                                 <p><b>Release Year: </b>${movie.Year}</p>
                                 <p><b>Media Type: </b>${movie.Type}</p>
                             </div>
-                        </div>`
-    }).join('');
+                        </div>`;
+        }).join('');
 
     moviesWrapper.innerHTML = moviesHTML;
-    loadingSpinner.classList.remove('movies__loading')
+    loadingSpinner.classList.remove('movies__loading');
+
 }
 
-async function searchMovies(event) {
-    const searchInput = event.target.value.trim().toLowerCase();
+async function searchMovies() {
+    const searchInput = document.getElementById('search').value.trim().toLowerCase();
     if (!searchInput) {
-        main();
+        moviesWrapper.innerHTML = "";
         return;
     }
     loadingSpinner.classList.add("movies__loading");
     moviesWrapper.innerHTML = '';
     const filteredMovies = await getMovies(searchInput);
     renderFilteredMovies(filteredMovies);
-    loadingSpinner.classList.remove("movies__loading");
+    loadingSpinner.classList.remove('movies__loading');
 }
 
-function renderFilteredMovies (filteredMovies) {
-    
+function renderFilteredMovies(filteredMovies) {
+
     if (!filteredMovies || filteredMovies.length === 0) {
         moviesWrapper.innerHTML = '<p class = "search__fail">No movies found. Try another search.</p>';
-        return; 
+        return;
     }
 
     const moviesHTML = filteredMovies.map((movie) => {
@@ -85,7 +83,7 @@ function renderFilteredMovies (filteredMovies) {
                                 <p><b>Release Year: </b>${movie.Year}</p>
                                 <p><b>Media Type: </b>${movie.Type}</p>
                             </div>
-                        </div>`
+                        </div>`;
     }).join('');
 
     moviesWrapper.innerHTML = moviesHTML;
@@ -96,7 +94,6 @@ function main() {
     searchInput.addEventListener('input', searchMovies);
     const filter = document.getElementById('filter');
     filter.addEventListener('change', filterMovies);
-    renderMovies();
 }
 
 main();
